3ef00ec90acf5ec3cc773d4e93615fd9ccc59d71
Michael Bychkowski <mbychkowski@google.com>
2023-03-15 21:44:39 +0000

    Code change

--- apps/backend00/main.go
+++ apps/backend00/main.go
@@ -20,7 +20,7 @@ func main() {
 	}
 
 	r.GET("/", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"body": "hello, world! :)"})
+    c.JSON(http.StatusOK, gin.H{"body": "hello, world! :("})
   })
 
 	r.GET("/albums", album.GetAlbums)
