a05f3b8f325fea6a458c38872cb20739cfaadd62
Michael Bychkowski <mbychkowski@google.com>
2024-07-09 21:02:41 +0000

    Update skaffold version

--- apps/backend00/main.go
+++ apps/backend00/main.go
@@ -34,7 +34,7 @@ func main() {
 	}
 
 	r.GET("/", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"body": "hello, world. [ + 🎉🎉🎉 + ]"})
+    c.JSON(http.StatusOK, gin.H{"body": "hello, world. [ + 😈 + ]"})
   })
 
 	r.GET("/albums", album.GetAlbums)
